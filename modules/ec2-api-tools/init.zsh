9f239c9426e1d55ac5d0ce2e5ac5f66aa54df032
Przybylski 중광 <r351574nc3@gmail.com>
2014-08-11 15:53:42 -0700

    Updating my defaults

--- modules/ec2-api-tools/init.zsh
+++ modules/ec2-api-tools/init.zsh
@@ -27,7 +27,7 @@ fi
 export JAVA_HOME="$(/usr/libexec/java_home)"
 export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
 export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
-export EC2_HOME=$(find /usr/local/Cellar/ec2-api-tools -type d -name libexec)
+export EC2_HOME=$(find /usr/local/ec2-api-tools -type d -name libexec)
 
 #
 # Aliases
